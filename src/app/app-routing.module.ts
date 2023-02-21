9f9ade5556a57f3651d2915353dc2ef18d103a9a
mihaiandreineacsu <mihai@developerakademie.com>
2023-02-21 20:52:02 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: '', component: ImprintComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
