158af16d721a064bd1aa51189ba4604c0dc12874
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-05-01 13:40:43 +0200

    add join project

--- src/app/routing/components/landing/introduction/introduction.animations.ts
+++ src/app/routing/components/landing/introduction/introduction.animations.ts
@@ -4,17 +4,21 @@ export const toggleMeAnimation = trigger(
     'visibleHidden', [
     state('visible', style({
         opacity: '1',
-        'z-index': '-1'
+        'z-index': '-1',
+        // width: '100%',
+        height: '50%'
     })),
     state('hidden', style({
         opacity: '0',
-        'z-index': '-1'
+        'z-index': '-1',
+        width: 0,
+        height: 0
     })),
     transition('visible => hidden', [
-        animate('300ms')
+        animate('300ms 3000ms')
     ]),
     transition('hidden => visible', [
-        animate('300ms')
+        animate('300ms 3000ms')
     ]),
 ]
 );
@@ -28,10 +32,10 @@ export const toggleIntroAnimation = trigger(
         width: 0
     })),
     transition('visible => hidden', [
-        animate('300ms 600ms')
+        animate('300ms')
     ]),
     transition('hidden => visible', [
-        animate('300ms 600ms')
+        animate('300ms')
     ]),
 ]
 );
@@ -43,6 +47,23 @@ export const toggleButtonAnimation = trigger(
     })),
     state('hidden', style({
 
+    })),
+    transition('visible => hidden', [
+        animate('300ms 600ms')
+    ]),
+    transition('hidden => visible', [
+        animate('300ms 600ms')
+    ]),
+]
+);
+
+export const toggleCurveAnimation = trigger(
+    'visibleHiddenCurve', [
+    state('visible', style({
+        width: '1442px'
+    })),
+    state('hidden', style({
+        width: 0
     })),
     transition('visible => hidden', [
         animate('300ms 600ms')
--- src/app/routing/components/landing/introduction/introduction.component.ts
+++ src/app/routing/components/landing/introduction/introduction.component.ts
@@ -17,24 +17,24 @@ export class IntroductionComponent implements OnInit, AfterViewInit {
   get viewBox() { return `0 0 ${this.width} ${this.height}` };
 
   get curvePathD() {
-    return `M${(1442/1442)*this.width} ${(0/1079)*this.height}
-  V${(712.4/1079)*this.height}
-  C${(1417.8/1449)*this.width} ${(724.3/1079)*this.height}
-   ${(1393/1449)*this.width} ${(735.9/1079)*this.height}
-   ${(1367.7/1449)*this.width} ${(747.3/1079)*this.height}
-  C${(1174.2/1449)*this.width} ${(834.1/1079)*this.height}
-   ${(943.8/1449)*this.width} ${(908.2/1079)*this.height}
-   ${(682.9/1449)*this.width} ${(967.3/1079)*this.height}
-  C${(404.9/1449)*this.width} ${(1030.3/1079)*this.height}
-   ${(152.1/1449)*this.width} ${(1063/1079)*this.height}
-   ${(2/1449)*this.width} ${(1078.6/1079)*this.height}
-  L${(0/1449)*this.width} ${(421.5/1079)*this.height}
-  C${(236/1449)*this.width} ${(441.5/1079)*this.height}
-   ${(353.4/1449)*this.width} ${(434.372/1079)*this.height}
-   ${(569.5/1449)*this.width} ${(385/1079)*this.height}
-  C${(984/1449)*this.width} ${(290.3/1079)*this.height}
-   ${(1281.7/1449)*this.width} ${(152.1/1079)*this.height}
-   ${(1442/1449)*this.width} ${(0/1079)*this.height}Z`;
+    return `M${(1442 / 1442) * this.width} ${(0 / 1079) * this.height}
+  V${(712.4 / 1079) * this.height}
+  C${(1417.8 / 1449) * this.width} ${(724.3 / 1079) * this.height}
+   ${(1393 / 1449) * this.width} ${(735.9 / 1079) * this.height}
+   ${(1367.7 / 1449) * this.width} ${(747.3 / 1079) * this.height}
+  C${(1174.2 / 1449) * this.width} ${(834.1 / 1079) * this.height}
+   ${(943.8 / 1449) * this.width} ${(908.2 / 1079) * this.height}
+   ${(682.9 / 1449) * this.width} ${(967.3 / 1079) * this.height}
+  C${(404.9 / 1449) * this.width} ${(1030.3 / 1079) * this.height}
+   ${(152.1 / 1449) * this.width} ${(1063 / 1079) * this.height}
+   ${(2 / 1449) * this.width} ${(1078.6 / 1079) * this.height}
+  L${(0 / 1449) * this.width} ${(421.5 / 1079) * this.height}
+  C${(236 / 1449) * this.width} ${(441.5 / 1079) * this.height}
+   ${(353.4 / 1449) * this.width} ${(434.372 / 1079) * this.height}
+   ${(569.5 / 1449) * this.width} ${(385 / 1079) * this.height}
+  C${(984 / 1449) * this.width} ${(290.3 / 1079) * this.height}
+   ${(1281.7 / 1449) * this.width} ${(152.1 / 1079) * this.height}
+   ${(1442 / 1449) * this.width} ${(0 / 1079) * this.height}Z`;
   }
 
   get gradientTransform() {
@@ -44,27 +44,51 @@ export class IntroductionComponent implements OnInit, AfterViewInit {
   toggleMe = 'hidden';
   constructor() { }
   ngAfterViewInit(): void {
-    setTimeout(()=>  this.toggleMe = 'visible', 200);
-    this.updateCurve();
+    setTimeout(() => this.toggleMe = 'visible', 200);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  /**
+   * Primarily used to updateCurve
+   * @param event
+   */
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.updateCurve();
   }
 
+  /**
+   * Out of use.
+   */
   updateCurve() {
-
     let timeout = setTimeout(() => {
       clearTimeout(timeout);
-      if(window.innerWidth > 1442)
+      if (window.innerWidth > 1442)
         this.width = window.innerWidth;
       else
         this.width = 1442;
     }, 100)
   }
 
+  /**
+   * Testing
+   */
+  randomizePath() {
+    const paths = Array.from(document.querySelectorAll('svg.shift-form path')) as SVGPathElement[];
+
+    paths.forEach(path => {
+      let d = path.getAttribute('d')?.split(' ').map(part => {
+        // Convert part to a number before checking if it is NaN
+        const numPart = parseFloat(part);
+        if (!isNaN(numPart)) {
+          return numPart + (Math.random() - 0.5) * 10; // Adjust range of randomness as needed
+        }
+        return part;
+      }).join(' ');
+      if (d) {
+        path.setAttribute('d', d);
+      }
+    })
+  }
 }
