54eb2002c7d15a00622cdd055410b69aac62d251
mihaiandreineacsu <mihai@developerakademie.com>
2025-06-20 14:17:46 +0200

    chore: setup linting scss

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: ['stylelint-config-standard-scss'],
+  overrides: [
+    {
+      files: ['**/*component.scss'], // Adjust the pattern to match your file(s)
+      rules: {
+        'no-empty-source': null, // Disable the rule for empty sources
+      },
+    },
+  ],
+};
